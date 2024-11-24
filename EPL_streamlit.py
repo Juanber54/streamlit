fc17b8c48a7dcda087b3a5c01a5938784f146bf7
Juanber54 <juan.semadeni@iteso.mx>
2024-11-23 21:19:45 -0600

    Update EPL_streamlit.py

--- EPL_streamlit.py
+++ EPL_streamlit.py
@@ -16,6 +16,51 @@ st.subheader(":violet[Análisis estadístico de diferentes parámetros ofensivos
 st.sidebar.image(Logo, width = 200)
 st.sidebar.markdown("## MENÚ DE CONFIGURACIÓN")
 
+#Gráfica 2 y 3
+vars_team1 = ['Arsenal','Liverpool','Aston Villa','Fulham','Newcastle United','Brentford','Tottenham Hotspur','Nottingham Forest','Manchester United','West Ham United','Wolverhampton Wanderers','Chelsea','Luton Town','Manchester City','Brighton and Hove Albion','Burnley','Everton','Crystal Palace','Bournemouth','Sheffield United']
+default_team1 = vars_team1.index('Liverpool')
+team1_selected = st.sidebar.selectbox('Elección del Equipo para el Gráfico:', vars_team1, index = default_team1)
+st.sidebar.divider()
+
+vars_team2 = ['Arsenal','Liverpool','Aston Villa','Fulham','Newcastle United','Brentford','Tottenham Hotspur','Nottingham Forest','Manchester United','West Ham United','Wolverhampton Wanderers','Chelsea','Luton Town','Manchester City','Brighton and Hove Albion','Burnley','Everton','Crystal Palace','Bournemouth','Sheffield United']
+default_team2 = vars_team2.index('Liverpool')
+team_selected2 = st.sidebar.selectbox('Elección del Equipo para el Gráfico:', vars_team2, index = default_team2)
+st.sidebar.divider()
+
+#Gráfica 4
+vars_top = st.sidebar.slider('Cantidad de jugadores mostrados: ', 1, 9)
+st.sidebar.divider()
+
+#Gráfica 4 y 11
+vars_stat = ['Shots per 90','Shot Conversion Rate (%)','Minutes','Matches','Expected Goals per 90','Goals per 90','Goals','Shots on Target per 90','Shot Accuracy (%)','90s','Total Shots','Total Shots on Target','Expected Goals']
+default_stat = vars_stat.index('Goals')
+stat_selected = st.sidebar.selectbox('Elección de los Parámetros para el Gráfico:', vars_stat, index = default_stat)
+st.sidebar.divider()
+
+#Gráfica 11
+selec_val_corr = st.sidebar.radio("Valores de Correlación:", options = ['Activo', 'Inactivo'])
+if selec_val_corr == 'Activo':
+    anotacion = True
+elif selec_val_corr == 'Inactivo':
+    anotacion = False
+
+#Gráfica 11
+stat_multi_selected = st.sidebar.multiselect('Parámetros de la Matriz de Correlación:', vars_stat, default = vars_stat)
+
+#Gráfica 11
+vars_cmap = ['viridis', 'plasma', 'inferno', 'magma', 'cividis', 'Greys', 'Purples', 'Blues', 'Greens', 'Oranges',
+             'Reds', 'YlOrBr', 'YlOrRd', 'OrRd', 'PuRd', 'RdPu', 'BuPu', 'GnBu', 'PuBu', 'YlGnBu', 'PuBuGn', 'BuGn',
+             'YlGn', 'binary', 'gist_yarg', 'gist_gray', 'gray', 'bone', 'pink', 'spring', 'summer', 'autumn', 'winter',
+             'cool', 'Wistia', 'hot', 'afmhot', 'gist_heat', 'copper', 'PiYG', 'PRGn', 'BrBG', 'PuOr', 'RdGy', 'RdBu',
+             'RdYlBu', 'RdYlGn', 'Spectral', 'coolwarm', 'bwr', 'seismic', 'twilight', 'twilight_shifted', 'hsv',
+             'Pastel1', 'Pastel2', 'Paired', 'Accent', 'Dark2', 'Set1', 'Set2', 'Set3', 'tab10', 'tab20', 'tab20b',
+             'tab20c', 'flag', 'prism', 'ocean', 'gist_earth', 'terrain', 'gist_stern', 'gnuplot', 'gnuplot2', 'CMRmap',
+             'cubehelix', 'brg', 'gist_rainbow', 'rainbow', 'jet', 'turbo', 'nipy_spectral', 'gist_ncar']
+color_selected = st.sidebar.selectbox('Paleta de Color para la Matriz de Correlación:', vars_cmap)
+if st.sidebar.button('Color Aleatorio') == True:
+    color_selected = random.choice(vars_cmap)
+
+#PANEL CENTRAL
 players = pd.read_csv('./Datos/EPL_df.csv')
 st.markdown("**DATAFRAME PARA EL ANÁLISIS DE LA MEJOR LIGA DEL MUNDO**")
 
