7d145fe61b5bca584321f4d6a5327747e49f8309
Juanber54 <juan.semadeni@iteso.mx>
2024-11-23 21:12:50 -0600

    Update EPL_streamlit.py

--- EPL_streamlit.py
+++ EPL_streamlit.py
@@ -17,13 +17,13 @@ st.sidebar.image(Logo, width = 200)
 st.sidebar.markdown("## MENÚ DE CONFIGURACIÓN")
 
 players = pd.read_csv('./Datos/EPL_df.csv')
-st.markdown(":blue[**DATAFRAME PARA EL ANÁLISIS DE LA MEJOR LIGA DEL MUNDO**]")
+st.markdown("**DATAFRAME PARA EL ANÁLISIS DE LA MEJOR LIGA DEL MUNDO**")
 
-st.markdown(":blue[Este DataFrame contiene información sobre las estadísticas de 240 jugadores de la EPL 23/24]")
+st.markdown(":blue[Este DataFrame contiene información sobre las estadísticas de 240 jugadores de la EPL 23/24.]")
 st.markdown(":blue[Sin embargo, se debe resaltar que la base de datos puede tener cierto sesgo, "
 "pues falta un porcentaje importante de los jugadores totales que estuvieron registrados durante esa temporada.]")
-st.markdown(":blue[Esto se debe principalmente a varios jugadores no registraron prácticamente ningún dato "
-"en los parámetros ofensivos que estamos considerando en este análisis]")
+st.markdown(":blue[Esto se debe principalmente a que varios jugadores no registraron prácticamente ningún dato "
+"en los parámetros ofensivos que estamos considerando en este análisis.]")
 
 st.dataframe(players)
 st.divider()
